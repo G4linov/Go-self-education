ade2c1f26fbe85ce87c7b818b6055d7c9a9b1b6a
Vyacheslav Galinov <galin0ff@yandex.ru>
2023-12-06 13:53:16 +0300

    homew

--- src/les1_hm/main.go
+++ src/les1_hm/main.go
@@ -1,14 +1,38 @@
-/*
-
-
-
- */
-
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
-	fmt.Println()
+	// Математические операции
+	var a = 1
+	var b = 3
+	var c = b - a
+	fmt.Println(c)
+	c = b * a
+	fmt.Println(c)
+	a++
+	b--
+	c = a % b
+	fmt.Println(a, b, c)
+
+	// Преобразование типов
+
+	i, _ := strconv.Atoi("-42")
+	s := strconv.Itoa(-42)
+
+	fmt.Println(s, i)
+
+	// Домашка пункт 4
+
+	var code = "104"
+	var number = 35
+
+	codeint, _ := strconv.Atoi(code)
+	numberstr := strconv.Itoa(number)
+
+	fmt.Println(codeint, numberstr)
 
 }
